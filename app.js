d125186b035f7d5f2bcca7ea09795bdf9667d995
Manu The Blacker <emmanueladekplovi1@gmail.com>
2023-01-14 01:52:42 +0100

    Change app logo

--- app.js
+++ app.js
@@ -243,7 +243,7 @@ app.use(app_context + '/stylesheets', express.static(path.join(__dirname, 'publi
 app.use(app_context + '/fonts', express.static(path.join(__dirname, 'public/fonts')));
 app.use(app_context + '/javascripts', express.static(path.join(__dirname, 'public/javascripts')));
 app.use(app_context + '/lunr', express.static(path.join(__dirname, 'node_modules/lunr')));
-app.use(app_context + '/favicon.png', express.static(path.join(__dirname, 'public/favicon.png')));
+app.use(app_context + '/favicon.ico', express.static(path.join(__dirname, 'public/favicon.ico')));
 
 // serving static content
 app.use(express.static(path.join(__dirname, 'public')));
